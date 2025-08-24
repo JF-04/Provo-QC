c047056f26f8c6969039b7092e803d9edc11acfc
JF-04 <jdominguezruben@icloud.com>
2025-08-24 17:15:49 -0300

    Add files via upload

--- appprovoqc.py
+++ appprovoqc.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 st.set_page_config(page_title="Análisis H30", layout="wide")
 
-st.title("📊 Análisis H30 PIUTERRA")
+st.title("📊 Análisis H30")
 
 # ==================================
 # ENTRADAS
